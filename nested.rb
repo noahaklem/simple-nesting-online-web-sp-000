9c9d255f2dd917f42ab60d25fa9737fbddea034b
Noah  Klem <klem.noah@gmail.com>
2021-06-02 21:00:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 def hopper
 	programmer_hash = 
  		{
@@ -106,6 +106,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      :programmer_hash[:alan_kay][:known_for] = alans_new_info
+     binding.pry
      return programmer_hash[:alan_kay][:known_for][0]
 end
 
